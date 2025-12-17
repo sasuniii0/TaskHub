378d32997998330b7f57230ee01a505fe4e1b606
sasuniii0 <sasuniwijerathne@gmail.com>
2025-12-17 09:26:55 +0530

    feat: Refactor the application with removing unused files in app and make sure the authentication file is correct or not

---
+++ app/login.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native"
+
+const Login = () => {
+    return (
+        <View>Login</View>
+    )
+}
+
+export default Login
---
+++ app/register.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native"
+
+const Register = () => {
+    return(
+        <View>Register</View>
+    )
+}
+
+export default Register
